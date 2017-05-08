64c2295774832e49f8e547443c7b8d7341c7b841
Chelder <chelder.lins@gmail.com>
2017-05-08 14:57:51 -0300

    Revisão maratona xamarin. Demo: Android

---
+++ MobileAppSamples/MaratonaXamarin.AndroidApp/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using MaratonaXamarin.Shared;
+using System;
+using System.Linq;
+
+namespace MaratonaXamarin.AndroidApp
+{
+    [Activity(Label = "MaratonaXamarin.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.btnCarregar);
+            var listView = FindViewById<ListView>(Resource.Id.lvwItens);
+
+            button.Click += async (sender, e) =>
+            {
+                var api = new UserApi();
+                var users = await api.ListAsync(new Developer
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Angelo",
+                    Email = "angelo@angelo.com",
+                    State = "SP",
+                    City = "Carapicuiba"
+                });
+
+                listView.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItemSingleChoice,
+                    users.OrderBy(u => u.Name).Select(u => $"{u.Id} {u.Name}").ToArray());
+            };
+        }
+    }
+}
+
---
+++ MobileAppSamples/MaratonaXamarin.Shared/UserApi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MaratonaXamarin.Shared
+{
+    public class UserApi
+    {
+        private readonly MobileServiceClient _client;
+        public UserApi()
+        {
+            _client = new MobileServiceClient("http://hellomonkeys.azurewebsites.net/");            
+        }
+
+        public async Task<List<User>> ListAsync(Developer developer)
+        {
+            var users = await _client.InvokeApiAsync<Developer, List<User>>("monkeys", developer);
+
+            return users;
+        }
+    }
+}
